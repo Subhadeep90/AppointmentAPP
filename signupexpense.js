dd80b47dc24bed360be62bd3422cd401e8813546
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-06-01 14:02:10 +0530

    loginbackend

--- signupexpense.js
+++ signupexpense.js
@@ -5,13 +5,50 @@ const path=require('path');
 app.use(cors());
 const bodyparser=require('body-parser'); 
 const { AsyncResource } = require('async_hooks');
-const sequelize=require('./Model/expenselogin')
+const sequelize=require('./Model/expenselogin');
+const { where } = require('sequelize');
 app.use(bodyparser.json({extended:false}));
 app.use('/user/Login',async(req,res)=>{
-res.sendStatus(200);
-})
+console.log(req.body)
+const Emailid=req.body.Email;
+const UserPassword=req.body.Password;
+if(Emailid=="" || UserPassword=="")
+{
+res.status(400).json({message:"Email or Password is missing"})
+}
+else{
+    try{
+       const user=await sequelize.findAll({
+    where:{Email:Emailid}})
 
+if(user.length>0)
+{
+    if(user[0].Password===UserPassword)
+    {
+      res.status(200).json({message:"User logged in sucessful"})
+    }
+    else
+    {
+    res.status(401).json({message:"Wrong Password"})
+    }
+}
+else{
+    return res.status(404).json({message:"User not found"})
+}
+}
+
+catch(error){
+res.status(505).json({message:"Something went wrong"})
+}
+
+}
+})
 app.use('/user/Signup',async(req,res)=>{
+if(req.body.Name==""||req.body.Email==""||req.body.Password=="")
+{
+    res.status(400).json({message:"Name or Email or Password is missing"})
+}
+else{
 try{
     await sequelize.create({
     Name:req.body.Name,
@@ -24,6 +61,7 @@ try{
 catch(error){
 console.log(error)
 res.send('User Already Exists') 
+}
 }
 })
 sequelize.sync().then((result)=>{
