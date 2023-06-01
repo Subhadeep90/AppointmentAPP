1da65becc38d6b7124aa606680ecb3eeb85299cf
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-06-01 21:47:11 +0530

    Passwordencryption

--- signupexpense.js
+++ signupexpense.js
@@ -6,6 +6,7 @@ app.use(cors());
 const bodyparser=require('body-parser'); 
 const { AsyncResource } = require('async_hooks');
 const sequelize=require('./Model/expenselogin');
+const bcrypt=require('bcrypt')
 const { where } = require('sequelize');
 app.use(bodyparser.json({extended:false}));
 app.use('/user/Login',async(req,res)=>{
@@ -23,14 +24,23 @@ else{
 
 if(user.length>0)
 {
-    if(user[0].Password===UserPassword)
-    {
-      res.status(200).json({message:"User logged in sucessful"})
-    }
+    bcrypt.compare(UserPassword,user[0].Password,(err,result)=>{
+        if(err)
+        {
+            throw new Error("Something went wrong")
+        }
+        if(result===true)
+        {
+        res.status(200).json({message:"User logged in sucessful"})
+        }
+    
+    
+
     else
     {
     res.status(401).json({message:"Wrong Password"})
     }
+})
 }
 else{
     return res.status(404).json({message:"User not found"})
@@ -50,12 +60,19 @@ if(req.body.Name==""||req.body.Email==""||req.body.Password=="")
 }
 else{
 try{
-    await sequelize.create({
-    Name:req.body.Name,
-    Email:req.body.Email,
-    Password:req.body.Password
+    const Passwordtaken=req.body.Password;
+    const saltrounds=10;
+    bcrypt.hash(Passwordtaken,saltrounds,async(error,hash)=>{
+        console.log(error)
+        await sequelize.create({
+            Name:req.body.Name,
+            Email:req.body.Email,
+            Password:hash
+        
+        })
+    })
 
-})
+    
 }
 
 catch(error){
